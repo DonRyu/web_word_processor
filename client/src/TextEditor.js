16f45afa7763b3e2e1b6ba7579cada0aa932fdb3
DonRyu <alpharyu1031@gmail.com>
2023-05-07 17:17:18 -0700

    add pdf word

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,10 +1,13 @@
-import { useCallback, useEffect, useState } from "react"
-import Quill from "quill"
-import "quill/dist/quill.snow.css"
-import { io } from "socket.io-client"
-import { useParams } from "react-router-dom"
+import { useCallback, useEffect, useState } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
+import { io } from "socket.io-client";
+import { useParams } from "react-router-dom";
+import { saveAs } from "file-saver";
+import * as quillToWord from "quill-to-word";
+import { pdfExporter } from "quill-to-pdf";
 
-const SAVE_INTERVAL_MS = 2000
+const SAVE_INTERVAL_MS = 2000;
 const TOOLBAR_OPTIONS = [
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
   [{ font: [] }],
@@ -15,85 +18,104 @@ const TOOLBAR_OPTIONS = [
   [{ align: [] }],
   ["image", "blockquote", "code-block"],
   ["clean"],
-]
+];
 
 export default function TextEditor() {
-  const { id: documentId } = useParams()
-  const [socket, setSocket] = useState()
-  const [quill, setQuill] = useState()
+  const { id: documentId } = useParams();
+  const [socket, setSocket] = useState();
+  const [quill, setQuill] = useState();
 
   useEffect(() => {
-    const s = io("http://localhost:3001")
-    setSocket(s)
+    const s = io("http://localhost:3001");
+    setSocket(s);
 
     return () => {
-      s.disconnect()
-    }
-  }, [])
+      s.disconnect();
+    };
+  }, []);
 
   useEffect(() => {
-    if (socket == null || quill == null) return
+    if (socket == null || quill == null) return;
 
-    socket.once("load-document", document => {
-      quill.setContents(document)
-      quill.enable()
-    })
+    socket.once("load-document", (document) => {
+      quill.setContents(document);
+      quill.enable();
+    });
 
-    socket.emit("get-document", documentId)
-  }, [socket, quill, documentId])
+    socket.emit("get-document", documentId);
+  }, [socket, quill, documentId]);
 
   useEffect(() => {
-    if (socket == null || quill == null) return
+    if (socket == null || quill == null) return;
 
     const interval = setInterval(() => {
-      socket.emit("save-document", quill.getContents())
-    }, SAVE_INTERVAL_MS)
+      socket.emit("save-document", quill.getContents());
+    }, SAVE_INTERVAL_MS);
 
     return () => {
-      clearInterval(interval)
-    }
-  }, [socket, quill])
+      clearInterval(interval);
+    };
+  }, [socket, quill]);
 
   useEffect(() => {
-    if (socket == null || quill == null) return
+    if (socket == null || quill == null) return;
 
-    const handler = delta => {
-      quill.updateContents(delta)
-    }
-    socket.on("receive-changes", handler)
+    const handler = (delta) => {
+      quill.updateContents(delta);
+    };
+    socket.on("receive-changes", handler);
 
     return () => {
-      socket.off("receive-changes", handler)
-    }
-  }, [socket, quill])
+      socket.off("receive-changes", handler);
+    };
+  }, [socket, quill]);
 
   useEffect(() => {
-    if (socket == null || quill == null) return
+    if (socket == null || quill == null) return;
 
     const handler = (delta, oldDelta, source) => {
-      if (source !== "user") return
-      socket.emit("send-changes", delta)
-    }
-    quill.on("text-change", handler)
+      if (source !== "user") return;
+      socket.emit("send-changes", delta);
+    };
+    quill.on("text-change", handler);
 
     return () => {
-      quill.off("text-change", handler)
-    }
-  }, [socket, quill])
+      quill.off("text-change", handler);
+    };
+  }, [socket, quill]);
 
-  const wrapperRef = useCallback(wrapper => {
-    if (wrapper == null) return
+  const wrapperRef = useCallback((wrapper) => {
+    if (wrapper == null) return;
 
-    wrapper.innerHTML = ""
-    const editor = document.createElement("div")
-    wrapper.append(editor)
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
     const q = new Quill(editor, {
       theme: "snow",
       modules: { toolbar: TOOLBAR_OPTIONS },
-    })
-    q.disable()
-    q.setText("Loading...")
-    setQuill(q)
-  }, [])
-  return <div className="container" ref={wrapperRef}></div>
+    });
+    q.disable();
+    q.setText("Loading...");
+    setQuill(q);
+  }, []);
+
+  const downloadDocument = async () => {
+    const delta = quill.getContents();
+    const blob = await quillToWord.generateWord(delta, { exportAs: "blob" });
+    saveAs(blob, "document.docx");
+  };
+
+  const downloadPdf = async () => {
+    const delta = quill.getContents();
+    const blob = await pdfExporter.generatePdf(delta);
+    saveAs(blob, "pdf-export.pdf");
+  };
+
+  return (
+    <>
+      <div className="container" ref={wrapperRef}></div>
+      <button onClick={downloadDocument}>Download as DOC</button>
+      <button onClick={downloadPdf}>Download as PDF</button>
+    </>
+  );
 }
