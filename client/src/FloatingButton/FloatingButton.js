ad790e321a069d976c67ebe330e81f7f82a784c3
DonRyu <alpharyu1031@gmail.com>
2023-05-07 17:46:31 -0700

    font awesome

--- client/src/FloatingButton/FloatingButton.js
+++ client/src/FloatingButton/FloatingButton.js
@@ -1,30 +1,31 @@
 import { useState } from "react";
 import "./FloatingButton.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function FloatingButton() {
-    const [showButtons, setShowButtons] = useState(false);
+  const [showButtons, setShowButtons] = useState(false);
 
-    const toggleButtons = () => {
-      setShowButtons(!showButtons);
-    };
-  
-    return (
-      <div className="floating-buttons">
-        <div className="button" onClick={toggleButtons}>
-          <i className="fa fa-plus"></i>
-        </div>
-        {showButtons && (
-          <div className="button-list">
-            <div className="button skyblue">
-              <i className="fa fa-pen"></i>
-            </div>
-            <div className="button skyblue">
-              <i className="fa fa-trash"></i>
-            </div>
-          </div>
-        )}
+  const toggleButtons = () => {
+    setShowButtons(!showButtons);
+  };
+
+  return (
+    <div className="floating-buttons">
+      <div className="button" onClick={toggleButtons}>
+        <i className="fa fa-plus"></i>
       </div>
-    );
+      {showButtons && (
+        <div className="button-list">
+          <div className="button skyblue">
+            <i className="fa fa-pen"></i>
+          </div>
+          <div className="button skyblue">
+          <FontAwesomeIcon icon="fa-solid fa-file-pdf" beat />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default FloatingButton;
